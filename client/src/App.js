5b01a72d9ab2660f01f6c6241cfd1d9155bb48ac
Griffin McManus <griffin@scrumadventures.com>
2022-07-05 17:50:38 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome ACSD20220705 Class!
+          Welcome ACSD20220705!
         </p>
       </header>
     </div>
