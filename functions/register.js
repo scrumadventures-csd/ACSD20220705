ce1911d8e6d0767ae4cec1333672e9fb3feea70d
ivanb9 <70458538+ivanb9@users.noreply.github.com>
2022-07-07 06:52:36 -0700

    Added request/response logging

--- functions/register.js
+++ functions/register.js
@@ -2,9 +2,10 @@ const axios = require("axios");
 
 const registerGame = async (frames=10,pins=10,rolls=2,test="") => {
   //call pinsetter
-  let myUrl=`http://pinsetter.herokuapp.com/pinsetter/?action=register&frames=${frames}&pins=${pins}&rolls=${rolls}&test=${test}`;
-  console.log("url = " + myUrl)
-  const res = await axios.get(myUrl);
+  let   myUrl = `http://pinsetter.herokuapp.com/pinsetter/?action=register&frames=${frames}&pins=${pins}&rolls=${rolls}&test=${test}`;
+  console.log  ("[REQUEST ] " + myUrl)
+  const res   = await axios.get(myUrl);
+  console.log  ("[RESPONSE] " + res.data)
   return res.data;
 };
 
