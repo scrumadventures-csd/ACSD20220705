027696402c110543404627b8df70f777fcce0b9c
ivanb9 <70458538+ivanb9@users.noreply.github.com>
2022-07-07 06:53:57 -0700

    Added request/response logging

--- functions/roll.js
+++ functions/roll.js
@@ -2,10 +2,11 @@ const axios = require("axios");
 
 const getRoll = async (id) => {
   //call pinsetter action roll
-  const res = await axios.get(
-    `http://pinsetter.herokuapp.com/pinsetter/?action=roll&id=${id}`
-  );
-  let pins = String(res.data);
+  let   myUrl  =`http://pinsetter.herokuapp.com/pinsetter/?action=roll&id=${id}`;
+  console.log   ("[REQUEST ] " + myUrl)
+  const res    = await axios.get (myUrl);
+  let   pins   = String(res.data);
+  console.log   ("[RESPONSE] " + pins)
   return pins;
 };
 
